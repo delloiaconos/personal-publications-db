1d17db8b76ed7beed4627548dc6e67f04f1274cf
Salvatore Dello Iacono <delloiaconos@gmail.com>
2025-07-06 16:17:28 +0200

    Added initial database schema.

---
+++ db.sql
@@ -0,0 +1,33 @@
+BEGIN TRANSACTION;
+CREATE TABLE IF NOT EXISTS "AuthorIdentifiers" (
+	"idAuthor"	INTEGER NOT NULL,
+	"IdentifierType"	TEXT NOT NULL,
+	"AuthorIdentifier"	TEXT NOT NULL UNIQUE
+);
+CREATE TABLE IF NOT EXISTS "Authors" (
+	"idAuthor"	INTEGER NOT NULL UNIQUE,
+	"FirstName"	TEXT NOT NULL,
+	"SecondName"	TEXT NOT NULL,
+	"MiddelName"	TEXT,
+	PRIMARY KEY("idAuthor" AUTOINCREMENT),
+	CONSTRAINT "fkAuthodsId_idAutor" FOREIGN KEY("idAuthor") REFERENCES "Authors"("idAuthor")
+);
+CREATE TABLE IF NOT EXISTS "DocumentAuthors" (
+	"idDocument"	INTEGER NOT NULL,
+	"idAuthor"	INTEGER NOT NULL,
+	"AuthOrder"	INTEGER DEFAULT 0,
+	CONSTRAINT "pkDocAuth" PRIMARY KEY("idDocument","idAuthor")
+);
+CREATE TABLE IF NOT EXISTS "DocumentIdentifiers" (
+	"idDocument"	INTEGER NOT NULL,
+	"IdentifierType"	TEXT NOT NULL,
+	"DocuemnttIdentifier"	TEXT NOT NULL UNIQUE,
+	CONSTRAINT "fkDocIde_idDoc" FOREIGN KEY("idDocument") REFERENCES ""
+);
+CREATE TABLE IF NOT EXISTS "Documents" (
+	"idDocument"	INTEGER NOT NULL UNIQUE,
+	"Title"	BLOB NOT NULL,
+	"DocumentType"	TEXT NOT NULL,
+	PRIMARY KEY("idDocument" AUTOINCREMENT)
+);
+COMMIT;
