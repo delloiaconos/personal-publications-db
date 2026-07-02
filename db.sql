c3eff2c9de5eda39d7190841fbe082e64fac2771
Salvatore Dello Iacono <delloiaconos@gmail.com>
2026-07-02 05:40:21 +0200

    Added DocumentTags table.

--- db.sql
+++ db.sql
@@ -1,5 +1,7 @@
 BEGIN TRANSACTION;
 
+-- Author's related tables
+
 CREATE TABLE IF NOT EXISTS "Authors" (
 	"idAuthor" INTEGER NOT NULL UNIQUE,
 	"FirstName" TEXT NOT NULL,
@@ -14,6 +16,8 @@ CREATE TABLE IF NOT EXISTS "AuthorIdentifiers" (
 	"AuthorIdentifier" TEXT NOT NULL UNIQUE,
 	CONSTRAINT "fkAuthorIdentifiers_idAuthor" FOREIGN KEY("idAuthor") REFERENCES "Authors"("idAuthor")
 );
+
+-- Document's related tables
 
 CREATE TABLE IF NOT EXISTS "Documents" (
 	"idDocument" INTEGER NOT NULL UNIQUE,
@@ -34,7 +38,6 @@ CREATE TABLE IF NOT EXISTS "DocumentIdentifiers" (
 CREATE INDEX "idxDocumentIdentifiers_IdentifierType" ON "DocumentIdentifiers" ("IdentifierType");
 CREATE INDEX "idxDocumentIdentifiers_DocumentIdentifier" ON "DocumentIdentifiers" ("DocumentIdentifier");
 
-
 CREATE TABLE IF NOT EXISTS "DocumentAuthors" (
 	"idDocument" INTEGER NOT NULL,
 	"idAuthor" INTEGER NOT NULL,
@@ -43,5 +46,14 @@ CREATE TABLE IF NOT EXISTS "DocumentAuthors" (
 	CONSTRAINT "fkDocumentAuthors_idAuthor" FOREIGN KEY("idAuthor") REFERENCES "Authors"("idAuthor"),
 	CONSTRAINT "fkDocumentAuthors_idDocument" FOREIGN KEY("idDocument") REFERENCES "Documents"("idDocument")
 );
+
+CREATE TABLE IF NOT EXISTS "DocumentTags" (
+	"idDocument" INTEGER NOT NULL,
+	"DocumentTag" TEXT NOT NULL,
+	CONSTRAINT "pkDocumentTags" PRIMARY KEY("idDocument","DocumentTag"),
+	CONSTRAINT "fkDocumentTags_idDocument" FOREIGN KEY("idDocument") REFERENCES "Documents"("idDocument")
+);
+
+CREATE INDEX "idxDocumentTags_DocumentTag" ON "DocumentTags" ("DocumentTag");
 
 COMMIT;
